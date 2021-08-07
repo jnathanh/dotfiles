5d1843c57156e7f27075155a788daf5e593ccf30
Nathan Hanna <nhanna@suplari.com>
2021-08-06 20:41:44 -0700

    more personalizations

---
+++ pip.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Install command-line tools using pip.
+
+# Make sure we’re using the latest pip.
+# brew update
+
+# Upgrade any already-installed packages.
+# brew upgrade
+
+# install pygments (required for json function)
+pip install pygments
