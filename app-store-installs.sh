7934840874151a40a58322e544090572b08c7d68
Nathan Hanna <jnathanh@gmail.com>
2023-12-13 23:04:20 -0800

    add mas app-store package installations

---
+++ app-store-installs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# requires mas install through brew first
+
+mas install 1440147259  # AdGuard for Safari
+mas install 497799835   # Xcode
+mas install 405399194   # Kindle
+mas install 1554029832  # MarkDownload - Markdown Web Clipper
+mas install 408981434   # iMovie
+mas install 409183694   # Keynote
+mas install 1586435171  # Actions; lots of useful Shortcuts actions
+mas install 1066144116  # Live Home 3D
+mas install 1435957248  # Drafts; text editor that integrates well with Shortcuts
+mas install 1480933944  # Vimari
+mas install 409201541   # Pages
+mas install 747648890   # Telegram
+mas install 682658836   # GarageBand
+mas install 409203825   # Numbers
+mas install 926036361   # LastPass
+# mas install 1453273600  # Data Jar - data store for Shortcuts
+# mas install 403504866 # PCalc - calculation/math actions for Shortcuts
